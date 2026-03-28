cf8e9e7e237b462e3ba7787a0496b2923f82bf00
Tom Bishop <114227637+TomBishop361@users.noreply.github.com>
2026-03-28 14:12:00 +0000

    Revert "Refine grapple behavior and jump velocity math"
    This reverts commit 9e734cacc0dd8bd0f505408444e558bf30b4f685.

--- Assets/Script/Helpers/MovementHelper_CS/JumpVelocityCalc.cs
+++ Assets/Script/Helpers/MovementHelper_CS/JumpVelocityCalc.cs
@@ -3,25 +3,14 @@ using UnityEngine;
 
 public static class JumpVelocityCalc 
 {
-    //Found math online
-    public static Vector3 CalculateJumpVelocity(Vector3 startPos, Vector3 endPos, float trajectoryHeight)
+    static public Vector3 CalculateJumpVelocity(Vector3 startPos, Vector3 endPos, float trajectoryHeight)
     {
         float gravity = Physics.gravity.y;
-        float displacementY = endPos.y - startPos.y;
-        Vector3 displacementXZ = new Vector3(endPos.x - startPos.x, 0f, endPos.z - startPos.z);
+        float YDisplacement = endPos.y - startPos.y;
+        Vector3 dispalcementXZ = new Vector3(endPos.x - startPos.x, 0f,endPos.z - startPos.z);  
 
-       
-        float terminalHeight = Mathf.Max(displacementY, 0) + 0.5f;
-        float actualHeight = Mathf.Max(trajectoryHeight, terminalHeight);
-
-  
-        Vector3 velocityY = Vector3.up * Mathf.Sqrt(-2 * gravity * actualHeight);
-
-     
-        float timeToPeak = Mathf.Sqrt(-2 * actualHeight / gravity);
-        float timeFromPeakToTarget = Mathf.Sqrt(2 * (displacementY - actualHeight) / gravity);
-
-        Vector3 velocityXZ = displacementXZ / (timeToPeak + timeFromPeakToTarget);
+        Vector3 velocityY = Vector3.up * Mathf.Sqrt(-2 * gravity * trajectoryHeight);
+        Vector3 velocityXZ = dispalcementXZ / (Mathf.Sqrt(-2 * trajectoryHeight / gravity) + Mathf.Sqrt(2 * (YDisplacement - trajectoryHeight) / gravity));
 
         return velocityXZ + velocityY;
     }
