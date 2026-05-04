f1794a9d625cb538cdbca88cf07583234a612f81
ArtJWMan <s4300909@glos.ac.uk>
2026-05-04 12:07:41 +0100

    PlayMain, PlayRelaxed and PlayIntense functions on MusicManager.cs

--- Assets/MusicManager.cs
+++ Assets/MusicManager.cs
@@ -4,8 +4,7 @@ public class MusicManager : MonoBehaviour
 {
     [SerializeField]
     AudioSource[] tracks;
-
-   
+    int trackIndex = 0;
 
     void Start()
     {
@@ -14,11 +13,58 @@ public class MusicManager : MonoBehaviour
         foreach (AudioSource track in tracks)
         {
             track.PlayScheduled(startTime);
+            track.mute = true;
+            trackIndex = trackIndex + 1;
+            Debug.Log(trackIndex);
         }
+        PlayMain();
     }
 
     private void OnValidate()
     {
         tracks = GetComponents<AudioSource>();
+    }
+
+    void PlayRelaxed()
+    {
+        trackIndex = 0;
+        foreach (AudioSource track in tracks)
+        {
+            if (trackIndex <= 21 && trackIndex >= 16)
+            {
+                track.mute = false;
+            }
+            trackIndex = trackIndex + 1;
+            Debug.Log(trackIndex);
+        }
+
+    }
+    void PlayMain()
+    {
+        trackIndex = 0;
+        foreach (AudioSource track in tracks)
+        {
+            if (trackIndex <= 15 && trackIndex >= 11 | trackIndex == 0)
+            {
+                track.mute = false;
+            }
+            trackIndex = trackIndex + 1;
+            Debug.Log(trackIndex);
+        }
+
+    }
+    void PlayIntense()
+    {
+        trackIndex = 0;
+        foreach (AudioSource track in tracks)
+        {
+            if (trackIndex <= 11 && trackIndex >= 0)
+            {
+                track.mute = false;
+            }
+            trackIndex = trackIndex + 1;
+            Debug.Log(trackIndex);
+        }
+
     }
 }
