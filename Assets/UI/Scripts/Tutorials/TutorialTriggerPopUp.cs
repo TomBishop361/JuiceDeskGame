fdffb1e35f3b47627cda73f861fdb9f9834bbdb5
GeorgeOugh <GeorgeOugh12@gmail.com>
2026-06-03 17:58:17 +0100

    Revert "Added in tutorials"
    This reverts commit 1a97a999c0de3a4898eb81472975ac0b703ef600.

--- Assets/UI/Scripts/Tutorials/TutorialTriggerPopUp.cs
+++ Assets/UI/Scripts/Tutorials/TutorialTriggerPopUp.cs
@@ -22,12 +22,4 @@ public class TutorialTriggerPopUp : MonoBehaviour
             tutorialPanel.SetActive(true);
         }
     }
-
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.tag == "Player")
-        {
-            tutorialPanel.SetActive(false);
-        }
-    }
 }
